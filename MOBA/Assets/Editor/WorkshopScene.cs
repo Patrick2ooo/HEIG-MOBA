325cbffd581ae6c697c9d17da50dcd344b856c94
Patrick2ooo <p.maillard1522@gmail.com>
2024-08-22 20:44:25 +0200

    readded WorkshopScene

---
+++ MOBA/Assets/Editor/WorkshopScene.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CustomBuild
+{
+    public static void PerformBuild()
+    {
+        // Specify the scenes you want to include in the build
+        string[] scenes = {
+            "Assets/Scenes/SampleScene.unity",  // Adjust the path as per your project structure
+        };
+
+        // Configure the build options
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions
+        {
+            scenes = scenes,
+            locationPathName = "Builds/StandaloneWindows64/MyGame.exe",  // Adjust the path as needed
+            target = BuildTarget.StandaloneWindows64,  // Specify the build target
+            options = BuildOptions.None
+        };
+
+        // Perform the build
+        BuildPipeline.BuildPlayer(buildPlayerOptions);
+    }
+}
