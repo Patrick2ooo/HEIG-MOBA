d6b5dc7e117032e0082ccd84720ec1447dc6abe0
Patrick2ooo <44113916+Patrick2ooo@users.noreply.github.com>
2024-08-26 10:52:13 +0200

    Added Editor bc of merge conflict

---
+++ MOBA/Assets/Editor/WorkshopScene.cs
@@ -0,0 +1,19 @@
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+
+public class PreBuildSetup : IPreprocessBuildWithReport
+{
+    public int callbackOrder => 0;
+
+    public void OnPreprocessBuild(BuildReport report)
+    {
+        // Ensure scenes are added to the build settings
+        EditorBuildSettingsScene[] scenes = new EditorBuildSettingsScene[]
+        {
+            new EditorBuildSettingsScene("Assets/Scenes/SampleScene.unity", true)
+        };
+
+        EditorBuildSettings.scenes = scenes;
+    }
+}
