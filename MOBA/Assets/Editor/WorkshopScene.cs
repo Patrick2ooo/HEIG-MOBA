c1524ead54ddc334827e97fb0f1ab1c59c479ba9
Patrick2ooo <44113916+Patrick2ooo@users.noreply.github.com>
2024-09-05 16:34:21 +0200

    Update WorkshopScene.cs

--- MOBA/Assets/Editor/WorkshopScene.cs
+++ MOBA/Assets/Editor/WorkshopScene.cs
@@ -11,7 +11,7 @@ public class PreBuildSetup : IPreprocessBuildWithReport
         // Ensure scenes are added to the build settings
         EditorBuildSettingsScene[] scenes = new EditorBuildSettingsScene[]
         {
-            //new EditorBuildSettingsScene("Assets/Scenes/UIs/MainMenu.unity", true),
+            new EditorBuildSettingsScene("Assets/Scenes/UIs/MainMenu.unity", true),
             new EditorBuildSettingsScene("Assets/Scenes/SampleScene.unity", true)
         };
 
