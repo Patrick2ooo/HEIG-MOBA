78cb73ac35afdbcff3d39d9218f79c1187c99094
Patrick2ooo <p.maillard1522@gmail.com>
2024-09-05 19:57:40 +0200

    debug build action

--- MOBA/Assets/Editor/WorkshopScene.cs
+++ MOBA/Assets/Editor/WorkshopScene.cs
@@ -2,30 +2,33 @@ using UnityEditor;
 using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
 
-public class PreBuildSetup : IPreprocessBuildWithReport
-{
-    public int callbackOrder => 0;
 
-    public void OnPreprocessBuild(BuildReport report)
+namespace WorkshopScene{
+    public static class PreBuildSetup : IPreprocessBuildWithReport
     {
-        // Ensure scenes are added to the build settings
-       // Log build report and scene setup
-        System.Console.WriteLine("Running PreBuildSetup. Modifying scenes for build.");
+        public int callbackOrder => 0;
 
-        // Ensure scenes are added to the build settings with MainMenu as the first scene
-        EditorBuildSettingsScene[] scenes = new EditorBuildSettingsScene[]
+        public static void OnPreprocessBuild(BuildReport report)
         {
-            new EditorBuildSettingsScene("Assets/Scenes/UIs/MainMenu.unity", true),  // MainMenu is the first scene
-            new EditorBuildSettingsScene("Assets/Scenes/SampleScene.unity", true)    // SampleScene is the second scene
-        };
+            // Ensure scenes are added to the build settings
+            // Log build report and scene setup
+            System.Console.WriteLine("Running PreBuildSetup. Modifying scenes for build.");
 
-        EditorBuildSettings.scenes = scenes;
+            // Ensure scenes are added to the build settings with MainMenu as the first scene
+            EditorBuildSettingsScene[] scenes = new EditorBuildSettingsScene[]
+            {
+                new EditorBuildSettingsScene("Assets/Scenes/UIs/MainMenu.unity", true),  // MainMenu is the first scene
+                new EditorBuildSettingsScene("Assets/Scenes/SampleScene.unity", true)    // SampleScene is the second scene
+            };
 
-        // Log each scene added to the build settings
-        foreach (var scene in scenes)
-        {
-            System.Console.WriteLine("Scene added to build: " + scene.path);
+            EditorBuildSettings.scenes = scenes;
+
+            // Log each scene added to the build settings
+            foreach (var scene in scenes)
+            {
+                System.Console.WriteLine("Scene added to build: " + scene.path);
+            }
         }
-    }
 
+    }
 }
