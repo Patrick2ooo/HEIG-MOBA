c75a89233814ed5465e3ac1037d898f1a5493b25
Patrick2ooo <p.maillard1522@gmail.com>
2024-09-05 21:05:39 +0200

    debug

--- MOBA/Assets/Editor/WorkshopScene.cs
+++ MOBA/Assets/Editor/WorkshopScene.cs
@@ -3,32 +3,22 @@ using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
 
 
-namespace WorkshopScene{
-    public static class PreBuildSetup
+
+public static class PreBuildSetup : IPreprocessBuildWithReport
+{
+    public int callbackOrder => 0;
+
+    public static void OnPreprocessBuild(BuildReport report)
     {
-        public int callbackOrder => 0;
-
-        public static void OnPreprocessBuild(BuildReport report)
+        // Ensure scenes are added to the build settings with MainMenu as the first scene
+        EditorBuildSettingsScene[] scenes = new EditorBuildSettingsScene[]
         {
-            // Ensure scenes are added to the build settings
-            // Log build report and scene setup
-            System.Console.WriteLine("Running PreBuildSetup. Modifying scenes for build.");
+            new EditorBuildSettingsScene("Assets/Scenes/UIs/MainMenu.unity", true),  // MainMenu is the first scene
+            new EditorBuildSettingsScene("Assets/Scenes/SampleScene.unity", true)    // SampleScene is the second scene
+        };
 
-            // Ensure scenes are added to the build settings with MainMenu as the first scene
-            EditorBuildSettingsScene[] scenes = new EditorBuildSettingsScene[]
-            {
-                new EditorBuildSettingsScene("Assets/Scenes/UIs/MainMenu.unity", true),  // MainMenu is the first scene
-                new EditorBuildSettingsScene("Assets/Scenes/SampleScene.unity", true)    // SampleScene is the second scene
-            };
-
-            EditorBuildSettings.scenes = scenes;
-
-            // Log each scene added to the build settings
-            foreach (var scene in scenes)
-            {
-                System.Console.WriteLine("Scene added to build: " + scene.path);
-            }
-        }
-
+        EditorBuildSettings.scenes = scenes;
     }
+
 }
+
